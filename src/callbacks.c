df80a94379870eac7a303730f7fe0efdaceb3462
Pierre Pronchery <khorben@defora.org>
2020-01-17 06:59:52 +0100

    Update the copyright notice

--- src/callbacks.c
+++ src/callbacks.c
@@ -1,6 +1,6 @@
 /* $Id$ */
 /* Copyright (c) 2010 Sébastien Bocahu <zecrazytux@zecrazytux.net> */
-/* Copyright (c) 2011-2013 Pierre Pronchery <khorben@defora.org> */
+/* Copyright (c) 2011-2020 Pierre Pronchery <khorben@defora.org> */
 /* This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, version 3 of the License.
